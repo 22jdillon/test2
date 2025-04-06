fdebddab23566dd652a2049e8efa2d5760dd21c7
22jdillon <22jdillon@wakatipu.school.nz>
2025-04-06 19:15:48 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 food_choices = [["takis", 7], ["milk", 6], ["butter", 8], ["eggs", 10], ["cheese", 12], ["cereal", 8], ["chicken", 10], ["steak", 20]]
-cart = [["beans", 2], ["toast", 7]]
+cart = []
 user = []
+addresses = []
 
 
 
 
 def menu():
+    print("")
     print("1)Shop")
     print("2)View cart")
     print("3)Checkout")
@@ -46,6 +48,7 @@ def print_food():
 
 def add():
     print_food()
+    print("")
     add = int(input("What product would you like add to your cart?\n> "))
     if add > 0 and add < (len(food_choices)+1):
         cart.append(food_choices[add-1])
@@ -56,11 +59,6 @@ def checkout():
     print_cart()
     print("\n1) Contine shopping.")
     print("2) Checkout.")
-    choice = int(input("> "))
-    if choice == 1:
-        print("")
-    if choice == 2:
-        print("still working")
 
 def user_info():
     print("1) Click and collect.") 
@@ -69,20 +67,69 @@ def user_info():
     if choice == 1:
         print("click and collect")
 
-def click_and_collect():
-    
-    user_name = input("Please enter your first and last name\n> ")
+def deliver():
+    name = input("Please enter your first and last name\n> ")
+    street = input("Please eneter your streets name\n> ")
+    house_number = input("Please enter your house number\n> ")
+    city = input("Enter your city\n> ")
+
+    address = [name, street, house_number, city]
+    addresses.append(address)
+
+def phone(prompt="Please enter your phone number\n> "):
+    while True:
+        try:
+            phone = int(input(prompt))
+            if len(str(phone)) > 10:
+                print("Invalid input, the phone number you have entered is more than 10 units long. Please try again.")
+            elif len(str(phone)) < 10:
+                print("Invalid input, the phone number you have entered is less than 10 units long. Please try again.")
+            else:
+                print("checkout")
+        except ValueError:
+            print("Invalid number. Please enter digits only.")
+
+def delivery_or_pickup():
+    print("Would you rather want to pick up your order or have it delivered?\n")
+    print("1) Click and Collect")
+    print("2) Deliver")
+
+def calculate_cost(user_cart):
+    cost = 0
+    for i in user_cart:
+        cost = cost + i[1]
+    return cost
 
 
-def phone():
-    phone = int(input("Please enter your phone number\n> (64+) "))
-    if len(str(phone)) > 10:
-        print("Invalid input, the phone number you have entered is more than 10 units long. Please try again.")
-    elif len(str(phone)) < 10:
-        print("Invalid input, the phone number you have entered is less than 10 units long. Please try again.")
-    elif len(str(phone)) == 10:
-        print("all good")
-    else:
-        print("idk")
 
-phone()
+
+def main():
+    while True:
+        menu()
+        choice = int(input("\nWhat would you like to do?\n> "))
+        if choice == 1:
+            add()
+        elif choice == 2:
+            print_cart()
+        elif choice == 3:
+            checkout()
+            while True:
+                choice = int(input("> "))
+                if choice == 1:
+                    break
+                elif choice == 2:
+                    while True:
+                        delivery_or_pickup()
+                        choice = int(input("> "))
+                        if choice == 1:
+                            print("Click and Collect")
+                        elif choice == 2:
+                            deliver()
+                            
+
+
+
+main()
+
+total_cost = calculate_cost(cart)
+print(f"The total cost of all your products is ${total_cost}")
